3142c30f7b9a0b2641ca44896bb91a347d8bf01f
Patipol Wangjaitham <patipol.wa@ku.th>
2017-06-05 17:20:08 +0700

    Add Todo

---
+++ app/src/main/java/com/example/max/memories/Todo.java
@@ -0,0 +1,15 @@
+package com.example.max.memories;
+
+/**
+ * Created by Max on 4/6/2560.
+ */
+
+public class Todo {
+    String desc;
+
+    public Todo(String desc){
+        this.desc = desc;
+    }
+
+    public String getDesc() { return desc; }
+}
