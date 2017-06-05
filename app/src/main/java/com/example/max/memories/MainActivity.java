12e592a095425aa518717f5c77a166a68887a31f
Patipol Wangjaitham <patipol.wa@ku.th>
2017-06-05 19:09:46 +0700

    Remove Textview

--- app/src/main/java/com/example/max/memories/MainActivity.java
+++ app/src/main/java/com/example/max/memories/MainActivity.java
@@ -27,19 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private TextView theDate;
     private Button btnGoCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        theDate = (TextView) findViewById(R.id.date);
         btnGoCalendar = (Button) findViewById(R.id.btnGoCalendar);
 
         Intent incomingIntent = getIntent();
         String date = incomingIntent.getStringExtra("date");
-        theDate.setText(date);
 
         dbHelper = new DbHelper(this);
         listTodo = (ListView)findViewById(R.id.listTodo);
