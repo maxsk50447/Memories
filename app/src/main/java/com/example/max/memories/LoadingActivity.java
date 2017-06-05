59c528f6664fb7cec6ae4e837698af6f048e2aa2
Patipol Wangjaitham <patipol.wa@ku.th>
2017-06-05 13:25:11 +0700

    Add Loading Activity

---
+++ app/src/main/java/com/example/max/memories/LoadingActivity.java
@@ -0,0 +1,46 @@
+package com.example.max.memories;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Max on 4/6/2560.
+ */
+
+public class LoadingActivity extends AppCompatActivity{
+    private Handler handler;
+    private Runnable runnable;
+    private long delayTime, time = 3000L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this, MainActivity.class));
+                finish();
+//                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        };
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, delayTime = time);
+        time = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delayTime - (System.currentTimeMillis() - time);
+    }
+}
